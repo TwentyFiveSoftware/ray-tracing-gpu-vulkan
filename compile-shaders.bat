f031eb1566e53159bf8dbc134e06d4d1ebcb434a
Water Chika <water_chika@outlook.com>
2024-03-29 08:12:03 -0700

    Generate shader spv by CMakeLists.txt

--- compile-shaders.bat
+++
@@ -1,6 +0,0 @@
-Pushd "%~dp0"
-
-"%VULKAN_SDK%\Bin\glslc.exe" "%~dp0shaders\shader.rgen" -o "cmake-build-debug\shaders\shader.rgen.spv" --target-env=vulkan1.2
-"%VULKAN_SDK%\Bin\glslc.exe" "%~dp0shaders\shader.rint" -o "cmake-build-debug\shaders\shader.rint.spv" --target-env=vulkan1.2
-"%VULKAN_SDK%\Bin\glslc.exe" "%~dp0shaders\shader.rchit" -o "cmake-build-debug\shaders\shader.rchit.spv" --target-env=vulkan1.2
-"%VULKAN_SDK%\Bin\glslc.exe" "%~dp0shaders\shader.rmiss" -o "cmake-build-debug\shaders\shader.rmiss.spv" --target-env=vulkan1.2
